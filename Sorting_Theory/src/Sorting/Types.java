e59d3ecf38b7d383e03fc216a2680e0865c2623b
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-11-18 22:10:56 +0530

    Add files via upload

---
+++ Sorting_Theory/src/Sorting/Types.java
@@ -0,0 +1,15 @@
+package Sorting;
+
+public class Types {
+
+	public static void main(String[] args) {
+		System.out.println("1.Bubble Sort");
+		System.out.println("2.Selection Sort");
+		System.out.println("3.Insertion Sort");
+		System.out.println("4.Merge Sort");
+		System.out.println("5.Quick Sort");
+		System.out.println("6.Heap Sort");
+		System.out.println("7.String Sort");
+	}
+
+}
---
+++ Sorting_Theory/src/Sorting/What_is_sorting.java
@@ -0,0 +1,12 @@
+package Sorting;
+
+public class What_is_sorting {
+
+	public static void main(String[] args) {
+		System.out.println("1.sorting is process of arranging the data in some logical order");             
+		System.out.println("2.in case of numeric values ,logical order may be in ascending or decending order.");                                                                                           
+		System.out.println("3.In case of alphanumeric values it can be in dictionary order.");
+
+	}
+
+}
