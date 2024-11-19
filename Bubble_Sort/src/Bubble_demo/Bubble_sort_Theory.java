3853bf1da35832f8437d0639a52d29f794cbde93
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-11-19 09:43:20 +0530

    Add files via upload

---
+++ Bubble_Sort/src/Bubble_demo/Bubble_sort_Theory.java
@@ -0,0 +1,11 @@
+package Bubble_demo;
+public class Bubble_sort_Theory {
+
+	public static void main(String[] args) {
+		System.out.println("1.Bubble  sort,also refer to sinking sort.");
+        System.out.println("2.It is a comparison based algorithm i.e. is a comparison each pair of adjacent items and swapping them if they are in the wrong order.");
+        System.out.println("3.The pass through the list is repeated until no swaps are needed,which indicated that the list is order.");
+
+	}
+
+}
