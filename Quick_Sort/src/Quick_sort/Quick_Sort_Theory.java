7d9432eec0427f62bc28ef0b86f8fcd0b78e45a5
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-11-24 10:38:08 +0530

    Add files via upload

---
+++ Quick_Sort/src/Quick_sort/Quick_Sort_Theory.java
@@ -0,0 +1,17 @@
+package Quick_sort;
+
+public class Quick_Sort_Theory {
+
+	public static void main(String[] args) {
+		System.out.println("Quick Sort");
+		System.out.println("1.sometimes it called as partition-exchnage sort");
+		System.out.println("2.it is an efficient sorting algorithm.");
+		System.out.println("3.serving as systematic method for placing elements of an array in order.");
+		System.out.println("4.it uses divide-conquer combine");
+		System.out.println("\t i.Divide-rearrange the elements and split the array into two subarrays and an element(known as pivot) in between such that each elements in left subarray is less than or equal to pivot element and each element in right subarray is greter than the pivot element.");
+		System.out.println("\t ii.combine-the solution of subproblems to create a solution to the original problem.");
+		System.out.println("\t iii.conquer:Recursively sort the two subarrays.");
+
+	}
+
+}
